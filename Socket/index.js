b62f42e97fed5825f268983ea733805422719309
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-06-12 22:30:29 +0530

    Socket Setup Done

--- Socket/index.js
+++ Socket/index.js
@@ -1,7 +1,14 @@
+const { Socket } = require('dgram')
+
 var app=require('express')()
 var http=require('http').createServer(app)
+var io=require('socket.io')(http)
+
 app.get('/',(req,res)=>{
-  res.send('<h1>Hello world</h1>')
+  res.sendFile(__dirname+'/index.html')
+})
+io.on('connection',(socket)=>{
+  console.log('a user connected')
 })
 
 http.listen(3000,()=>{
