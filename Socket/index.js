43de2cfc34632f9576b71e7c9e2b4a33498699b9
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-06-12 22:16:39 +0530

    Server setup done

---
+++ Socket/index.js
@@ -0,0 +1,9 @@
+var app=require('express')()
+var http=require('http').createServer(app)
+app.get('/',(req,res)=>{
+  res.send('<h1>Hello world</h1>')
+})
+
+http.listen(3000,()=>{
+  console.log('Listening on 3000')
+})
