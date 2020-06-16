1beb0328685fa2f581887d53a0d3aeddc5d37b32
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-06-16 20:55:28 +0530

    Done

--- chatbox/server.js
+++ chatbox/server.js
@@ -1,51 +1,63 @@
-var express=require('express')
-var app=express()
-var http=require('http')(app)
-var path=require('path')
-var io=require('socket.io')(http)
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var path = require("path");
+var io = require('socket.io')(http);
 
-var onlineusers=[]
+var onlineUsers = [];
 
-app.use(express.static(path.join(__dirname,"public")))
-app.get('/',(req,res)=>{
-  res.sendFile(__dirname+'/public/view/chatPage.html')
-})
+app.use(express.static(path.join(__dirname, "public")));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/view/chatPage.html');
+});
 
-io.on('connection',(socket)=>{
-  socket.on('user name',(user,callback)=>{
-    var temp=0
-    onlineusers.push({
-      profileName:user.userName,
-      profileId:socket.id,
-      profileImage:user.imageUrl,
-      profileAge:user.userAge,
-      profileSchool:user.userSchool,
-      profileCity:user.city,
-      counter:temp
+io.on('connection', function(socket) {
+
+    //   console.log('a user connected');
+
+    socket.on('user name', function(user, callback) {
+        var temp = 0;
+        onlineUsers.push({
+            profileName: user.userName,
+            profileId: socket.id,
+            profileImage: user.imageUrl,
+            profileAge: user.userAge,
+            profileSchool: user.userSchool,
+            profileCity: user.userCity,
+            counter: temp
+        })
+
+        // console.log(userName);
+        console.log(onlineUsers);
+
+        io.sockets.emit('connectedUsers', onlineUsers);
+
+    });
+
+    socket.on('disconnect', function() {
+        var i = 0;
+        while (i < onlineUsers.length) {
+            if (onlineUsers[i].profileId == socket.id) {
+                break;
+            }
+            i++;
+        }
+        console.log(socket.id + 'disconnect');
+
+        onlineUsers.splice(i, 1);
+        io.sockets.emit('connectedUsers', onlineUsers);
+        //console.log('user disconnected');
+    });
+
+    socket.on('chatting', function(message, sender, receiver) {
+
+        socket.to(receiver).emit('reciverPeer', message, socket.id, receiver);
+        socket.emit('senderPeer', message, socket.id, receiver);
+
     })
-    console.log(onlineusers)
-    io.sockets.emit('connectedusers',onlineusers)
-  })
-})
-socket.on('disconnect',()=>{
-  var i=0
-  while(i<onlineusers.length){
-    if(onlineusers[i].profileId==socket.id){
-      break;
-    }
-    i++;
-  }
-  console.log(socket.id+'disconnect')
-  onlineusers.splice(i,1)
-  io.sockets.emit('connectedusers',onlineusers)
-})
 
-socket.on('chatting',(message,sender,receiver)=>{
-  socket.to(receiver).emit('reciverPeer',message,socket.id,receiver)
-  socket.emit('senderPeer',message,socket.io,receiver)
+});
 
-})
-
-http.listen(3000,()=>{
-  console.log("Server running on port 3000")
-})
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
